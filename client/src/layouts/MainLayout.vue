f88d35f7fd3723b3ca6763db952f525c9cc18d0d
wey <qaz237562@gmail.com>
2025-05-15 11:42:53 +0800

    refactor:把grid寫進樣式讓版面正常

--- client/src/layouts/MainLayout.vue
+++ client/src/layouts/MainLayout.vue
@@ -1,8 +1,45 @@
+<template>
+	<div class="layout">
+		<MainSidebar class="sidebar" />
+		<SubHeader class="header" />
+		<RouterView v-slot="{ Component }">
+			<component :is="Component" class="content" />
+		</RouterView>
+		<SubFooter class="footer" />
+	</div>
+</template>
+
 <script setup>
+import SubHeader from "../components/SubHeader.vue";
+import SubFooter from "@/components/SubFooter.vue";
 import MainSidebar from "../components/MainSidebar.vue";
 </script>
 
-<template>
-  <MainSidebar />
-  <RouterView />
-</template>
+<style scoped>
+.layout {
+	display: grid;
+	grid-template-areas:
+		"sidebar header"
+		"sidebar content"
+		"sidebar footer";
+	grid-template-columns: 1px 1fr;
+	grid-template-rows: 75px 1fr 75px;
+	height: 100vh;
+}
+
+.sidebar {
+	grid-area: sidebar;
+}
+
+.header {
+	grid-area: header;
+}
+
+.content {
+	grid-area: content;
+}
+
+.footer {
+	grid-area: footer;
+}
+</style>
--- client/src/views/Trending.vue
+++ client/src/views/Trending.vue
@@ -1,3 +1,7 @@
 <template>
-  <h1>Trending</h1>
+	<div class="content">
+		<h1>Trending</h1>
+		lorem1000
+	</div>
+	<!--內容一定要寫在div class="content"裡面!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 不然會跑掉-->
 </template>
