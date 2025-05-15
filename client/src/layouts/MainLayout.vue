f9044f2383fc61d18c05b67a532aebdbf3ae5014
wey <qaz237562@gmail.com>
2025-05-15 12:31:48 +0800

    chores:加入說明

--- client/src/layouts/MainLayout.vue
+++ client/src/layouts/MainLayout.vue
@@ -7,6 +7,7 @@
 		</RouterView>
 		<SubFooter class="footer" />
 	</div>
+	<!--routerview 內容一定要寫在div class="content"裡面!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 不然會跑掉-->
 </template>
 
 <script setup>
