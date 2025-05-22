285562da07fee184744e4a1f132ede8ac2efbcef
Vivi <33819714+VVSNTUA@users.noreply.github.com>
2025-05-22 21:51:46 +0800

    [feat] 抽出 Search.vue 頁的 svg icon 為獨立元件，以供重複使用

--- client/src/views/Search.vue
+++ client/src/views/Search.vue
@@ -1,6 +1,12 @@
 <script setup>
 import { ref, computed, watchEffect, onMounted } from "vue";
 import { useRoute, useRouter } from "vue-router";
+import YourWorkIcon from "@/components/icons/YourWorkIcon.vue";
+import PensIcon from "@/components/icons/PensIcon.vue";
+import ProjectsIcon from "@/components/icons/ProjectsIcon.vue";
+import CollectionIcon from "@/components/icons/CollectionIcon.vue";
+import LeftArrowIcon from "@/components/icons/LeftArrowIcon.vue";
+import RightArrowIcon from "@/components/icons/RightArrowIcon.vue";
 
 const route = useRoute();
 const router = useRouter();
@@ -194,7 +200,6 @@ const fakeData = {
   ],
 };
 
-
 // 初次載入時觸發搜尋
 onMounted(() => {
   const q = route.query.q || "";
@@ -207,11 +212,14 @@ const filteredCards = computed(() => {
   const keywords = searchKeyword.value.trim().toLowerCase().split(/\s+/);
 
   return allCards.value.filter((card) => {
-    const title = card.title.toLowerCase(); 
+    const title = card.title.toLowerCase();
     const description = card.description?.toLowerCase() || "";
-    const author = card.author?.toLowerCase() || ""
+    const author = card.author?.toLowerCase() || "";
     return keywords.every(
-      (keyword) => title.includes(keyword) || description.includes(keyword) || author.includes(keyword)
+      (keyword) =>
+        title.includes(keyword) ||
+        description.includes(keyword) ||
+        author.includes(keyword)
     );
   });
 });
@@ -228,7 +236,7 @@ const onSearchSubmit = () => {
     path: `/search/${route.params.category || "pens"}`,
     query: { q: inputKeyword.value },
   });
-}
+};
 
 // 計算總頁數
 const totalPages = computed(() =>
@@ -314,70 +322,37 @@ function prevPage() {
                 href="/your-work"
                 class="px-3 py-1 h-full rounded bg-[#4F5465] text-white text-sm hover:bg-[#5A5F73] transition transform active:translate-y-0.5 flex items-center"
               >
-                <svg
-                  viewBox="0 0 71 89"
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="fill-current w-3 mr-1.5"
-                >
-                  <path
-                    d="M67.783 66.757c-3.771-5.87-8.347-9.685-14.883-11.845 0 0-4.932-2.547-17.825-2.547S17.25 54.912 17.25 54.912C10.713 57.09 6.14 60.887 2.36 66.757.857 69.067.056 73.716 0 76.195c.02.645 0 1.391 0 2.14v4.28-2.954c0 4.734 3.216 8.568 7.188 8.568h55.756c3.972 0 7.188-3.834 7.188-8.568v2.955-4.28c0-.75-.01-1.496 0-2.14-.048-2.48-.86-7.129-2.35-9.439m-52.37-46.875c0 10.972 6.818 27.097 19.653 27.097 12.62 0 19.646-16.125 19.646-27.097C54.711 8.91 45.913 0 35.065 0 24.21 0 15.41 8.91 15.41 19.882"
-                  ></path>
-                </svg>
+                <YourWorkIcon class="fill-current w-3 mr-1.5" />
                 Your Work
               </a>
               <a
                 href="/search/pens?q="
                 class="px-3 py-1 rounded bg-[#4F5465] text-white text-sm hover:bg-[#5A5F73] transition transform active:translate-y-0.5 flex items-center"
               >
-                <svg
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
+                <PensIcon
                   class="fill-current w-3 mr-1.5"
                   :class="{ 'text-[#0EBEFF]': activeTab === 'pens' }"
-                >
-                  <path
-                    d="M0 9.002C0 8.45.455 8 .992 8h18.016c.548 0 .992.456.992 1.002v9.996c0 .553-.455 1.002-.992 1.002H.992C.444 20 0 19.544 0 18.998zm0-8C0 .45.451 0 .99 0h4.02A.99.99 0 0 1 6 1.003v4.994C6 6.551 5.549 7 5.01 7H.99A.99.99 0 0 1 0 5.997zm7 0C7 .45 7.451 0 7.99 0h4.02A.99.99 0 0 1 13 1.003v4.994C13 6.551 12.549 7 12.01 7H7.99A.99.99 0 0 1 7 5.997zm7 0C14 .45 14.451 0 14.99 0h4.02A.99.99 0 0 1 20 1.003v4.994C20 6.551 19.549 7 19.01 7h-4.02A.99.99 0 0 1 14 5.997z"
-                  ></path>
-                </svg>
+                />
                 Pens
               </a>
               <a
                 href="/search/projects?q="
                 class="px-3 py-1 rounded bg-[#4F5465] text-white text-sm hover:bg-[#5A5F73] transition transform active:translate-y-0.5 flex items-center"
               >
-                <svg
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
+                <ProjectsIcon
                   class="fill-current w-3 mr-1.5"
                   :class="{ 'text-[#FFDD40]': activeTab === 'projects' }"
-                >
-                  <path
-                    d="M7 .995C7 .445 7.447 0 7.999 0H19c.552 0 .999.456.999.995v8.01c0 .55-.447.995-.999.995H8C7.447 10 7 9.544 7 9.005zm0 11.003c0-.551.447-.998.999-.998H19c.552 0 .999.446.999.998v7.004c0 .551-.447.998-.999.998H8A.998.998 0 0 1 7 19.002zM0 .992C0 .444.451 0 .99 0h4.02c.546 0 .99.455.99.992v18.016a.996.996 0 0 1-.99.992H.99a.999.999 0 0 1-.99-.992z"
-                  ></path>
-                </svg>
+                />
                 Projects
               </a>
               <a
                 href="/search/collections?q="
                 class="px-3 py-1 rounded bg-[#4F5465] text-white text-sm hover:bg-[#5A5F73] transition transform active:translate-y-0.5 flex items-center"
               >
-                <svg
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
+                <CollectionIcon
                   class="fill-current w-3 mr-1.5"
                   :class="{ 'text-[#AE63E4]': activeTab === 'collections' }"
-                >
-                  <rect height="9.64" rx="1" width="9.64" y="10.36"></rect>
-                  <rect
-                    height="9.64"
-                    rx="1"
-                    width="9.64"
-                    x="10.36"
-                    y="10.36"
-                  ></rect>
-                  <rect height="9.64" rx="1" width="9.64"></rect>
-                  <rect height="9.64" rx="1" width="9.64" x="10.36"></rect>
-                </svg>
+                />
                 Collections
               </a>
             </div>
@@ -408,15 +383,7 @@ function prevPage() {
                 @click="prevPage"
                 class="px-4 py-3 bg-[#444857] hover:bg-[#5A5F73] rounded-sm flex gap-1"
               >
-                <svg
-                  viewBox="-100.9 99.1 61.9 105.9"
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="fill-current w-2"
-                >
-                  <path
-                    d="m-98.2 158.8 43.5 43.5c1.7 1.7 4 2.7 6.5 2.7s4.8-1 6.5-2.7 2.7-4 2.7-6.5-1-4.8-2.7-6.5l-37.2-37.2 37.2-37.2c1.7-1.7 2.7-4 2.7-6.5s-1-4.8-2.6-6.5c-1.8-1.9-4.2-2.8-6.6-2.8-2.3 0-4.6.9-6.5 2.6-11.5 11.4-41.2 41-43.3 43l-.2.2c-3.6 3.6-3.6 10.3 0 13.9z"
-                  ></path>
-                </svg>
+                <LeftArrowIcon class="fill-current w-2" />
                 Prev
               </button>
               <button
@@ -425,15 +392,7 @@ function prevPage() {
                 class="px-4 py-3 bg-[#444857] hover:bg-[#5A5F73] rounded-sm mr-2 flex gap-1"
               >
                 Next
-                <svg
-                  viewBox="-100.9 99.1 61.9 105.9"
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="fill-current w-2"
-                >
-                  <path
-                    d="m-41.7 145.3-43.5-43.5c-1.7-1.7-4-2.7-6.5-2.7s-4.8 1-6.5 2.7-2.7 4-2.7 6.5 1 4.8 2.7 6.5L-61 152l-37.2 37.2c-1.7 1.7-2.7 4-2.7 6.5s1 4.8 2.6 6.5c1.8 1.9 4.2 2.8 6.6 2.8 2.3 0 4.6-.9 6.5-2.6 11.5-11.4 41.2-41 43.3-43l.2-.2c3.6-3.6 3.6-10.4 0-13.9z"
-                  ></path>
-                </svg>
+                <RightArrowIcon class="fill-current w-2" />
               </button>
             </nav>
           </div>
